a4d23e1ac2b17b230e2819c8ed6cbab7f15ffe0e
Rishi Raj <rishi.rajsbg@gmail.com>
2024-07-19 01:27:09 +0530

    Sending request to server

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ function handleListening (){
 
 //const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
+
+// function handleHome(){
+//     console.log(`Hii from Home`)
+// }
+function handleHome(req, res){
+    console.log(`Hii from Home`);
+    res.send("Hello from Home");
+}
+
+function handleProfile(req, res){
+    console.log(`Hii from Profile`);
+    res.send("You are on my Profile");
+}
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
 // respond with "hello world" when a GET request is made to the homepage
 // app.get('/', (req, res) => {
 //   res.send('hello world')

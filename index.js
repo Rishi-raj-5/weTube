e9ed721e4e30e2bf5eb44261d793ced114723425
Rishi Raj <rishi.rajsbg@gmail.com>
2024-07-19 01:12:41 +0530

    added server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hii");
+
+const express = require('express')
+const app = express()
+const PORT= 4000;
+
+function handleListening (){
+    console.log(`Listening on: http://localhost:${PORT}`)
+}
+
+//const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+
+// respond with "hello world" when a GET request is made to the homepage
+// app.get('/', (req, res) => {
+//   res.send('hello world')
+// })
+
+app.listen(PORT, handleListening);

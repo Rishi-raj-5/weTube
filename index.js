8e7faa89ba98ae08a2ab0855d6f5b9466b69e4d7
Rishi Raj <rishi.rajsbg@gmail.com>
2024-07-19 01:52:20 +0530

    Added Babel and nodemon

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-
-const express = require('express')
+import express from "express";
+// const express = require('express')
 const app = express()
 const PORT= 4000;
 
-function handleListening (){
-    console.log(`Listening on: http://localhost:${PORT}`)
-}
+// function handleListening (){
+//     console.log(`Listening on: http://localhost:${PORT}`)
+// }
 
-//const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
 
 // function handleHome(){
 //     console.log(`Hii from Home`)
 // }
-function handleHome(req, res){
-    console.log(`Hii from Home`);
-    res.send("Hello from Home");
-}
 
-function handleProfile(req, res){
-    console.log(`Hii from Profile`);
-    res.send("You are on my Profile");
-}
+// function handleHome(req, res){
+//     console.log(`Hii from Home`);
+//     res.send("Hello from Home");
+// }
+const handleHome = (req, res) => res.send("Hello from Home");
+
+
+// function handleProfile(req, res){
+//     console.log(`You are on my profile`);
+//     res.send("You are on my Profile");
+// }
+
+const handleProfile = (req, res) => res.send("You are on my Profile");
+
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 // respond with "hello world" when a GET request is made to the homepage

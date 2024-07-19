4d911f70eb7cb83f18a15708053761a10463545c
Rishi Raj <rishi.rajsbg@gmail.com>
2024-07-19 06:34:23 +0530

    added global/user/video routers

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import { userRouter } from "./router";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouters";
 const app = express();
 
-const handleHome = (req, res) => res.send("Hello from Home");
 
-
-const handleProfile = (req, res) => res.send("You are on my Profile");
 
 
 app.use(cookieParser());
@@ -18,17 +17,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-
-
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
-
+app.use("/", globalRouter);
 app.use("/user", userRouter);
-// app.use("/edit", userRouter);
-// app.use("/password", userRouter);
-
-
-
+app.use("/video", videoRouter);
 
 
 export default app;

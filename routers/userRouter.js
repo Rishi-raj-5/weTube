d8b31e2b9f9080617b9de371ee2eba4c208da223
Rishi Raj <rishi.rajsbg@gmail.com>
2024-07-19 07:03:12 +0530

    added global routes

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,7 @@
 import express from "express";
-
+import routes from "../routers";
 const userRouter = express.Router();
+
+
 
 export default userRouter;
--- routers/videoRouters.js
+++ routers/videoRouters.js
@@ -1,4 +1,5 @@
 import express from "express";
+import routes from "../routers";
 const videoRouter = express.Router();
 
 export default videoRouter;

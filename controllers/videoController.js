f90a053991525717a4cf9cca2b6f062c7d20b2c7
Rishi Raj <rishi.rajsbg@gmail.com>
2024-07-21 01:34:46 +0530

    added searchingby

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,21 @@
 export const home = (req, res) => res.render("home", {pageTitle: 'Home'}); 
-export const search = (req, res) => res.render("search",{pageTitle: 'Search'});
+
+export const search = (req, res) => {
+
+    //console.log(req.query.term);
+    //const searchingBy =req.query.term;
+    const { query : {term : searchingBy}
+ }= req;
+//  console.log(term);
+    res.render("search",{pageTitle: 'Search', searchingBy: searchingBy});
+}
 export const upload = (req, res) => res.render("upload",  {pageTitle: 'Upload'}); 
+
+
 export const editVideo = (req, res) => res.render("editVideo",  {pageTitle: 'Edit Video'});
+
 export const deleteVideo = (req, res) => res.render("deleteVideo",  {pageTitle: 'Delete Video'}); 
+
 export const videoDetail = (req, res) => res.render("videoDetail",  {pageTitle: 'Video Detail'});
 
  export const videos= (req, res) => res.render("videos");
